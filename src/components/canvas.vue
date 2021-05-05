6450e9b544b41db610c102061bccb5abcd0ad795
saransh86 <chhibber.saransh@gmail.com>
2021-05-05 16:04:20 -0500

    Test change

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="dark">
         <b-navbar-nav>
-        <b-navbar-brand href="#">Simple Graph</b-navbar-brand>
+        <b-navbar-brand href="#">Simple Graph Parina!</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
